170454e5ec730a84169d8589c4eca07707273152
adrianrevilla009 <adrianrg1996@gmail.com>
2022-12-01 16:40:30 +0100

    initial commit

---
+++ src/main/java/masterCloudApps/api/bookManagementAPI/BookManagementApiApplication.java
@@ -0,0 +1,13 @@
+package masterCloudApps.api.bookManagementAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookManagementApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookManagementApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/masterCloudApps/api/bookManagementAPI/BookManagementApiApplicationTests.java
@@ -0,0 +1,13 @@
+package masterCloudApps.api.bookManagementAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookManagementApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
